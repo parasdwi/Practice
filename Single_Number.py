aec74ea8e30f66b9b99deba4afd1639fa353faaa
PARAS <110665835+parasdwi@users.noreply.github.com>
2022-10-09 14:21:05 +0530

    Create Single_Number.py

---
+++ Single_Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        i,j=0,1
+        nums.sort()
+        while(j<len(nums)):
+            if nums[i]==nums[j]:
+                i+=2
+                j+=2
+            else:
+                return nums[i]
+        return nums[len(nums)-1]
