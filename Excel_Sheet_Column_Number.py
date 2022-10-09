3e77fadfb2375a88f24055439f81308cf5bf0894
PARAS <110665835+parasdwi@users.noreply.github.com>
2022-10-09 12:20:21 +0530

    Update Excel_Sheet_Column_Number.py

--- Excel_Sheet_Column_Number.py
+++ Excel_Sheet_Column_Number.py
@@ -1,7 +1,15 @@
-columnTitle="AC"
-i=0
-sum=0
-while (i<len(columnTitle)):
-    sum+= 26*(ord(columnTitle[i])-64)
-    i=i+1
-print(sum+ord(columnTitle[i])-64)
+# faster then 98.56% online submissions
+
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        i,sum=len(columnTitle)-1,0
+        j=0
+        while(0<=i<len(columnTitle)):
+            sum+=(26**(i))*(ord(columnTitle[j])-64)
+            j+=1
+            i-=1
+        return sum
